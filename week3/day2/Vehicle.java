42818728e931118c5c9226e59d6ec8f2500f5f0d
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-10-26 18:58:21 +0530

    Add files via upload

---
+++ week3/day2/Vehicle.java
@@ -0,0 +1,12 @@
+package week3.day2;
+
+public class Vehicle {
+	
+	public void applyBrake() {
+		System.out.println("brake is aplied---->superclass");
+	}
+	public void soundHorn() {
+		System.out.println("move----->superclass");
+	}
+	
+}
